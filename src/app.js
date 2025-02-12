a4b45aa78d380e76bc5522099bdfeaf6a091ed0e
Éric  António <ericantoniodev@gmail.com>
2025-02-12 15:54:56 +0200

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// import express from "express";
+
+// const app = express();
+// const PORT = 3333;
+
+// app.get("/ping", (req, res) => {
+//   res.json({ message: "pong" });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
